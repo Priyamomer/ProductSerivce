e8ad114478bde65f214655721799f6e19cd462ef
Priyam Omer <fsi@Priyams-MacBook-Pro.local>
2023-12-30 11:23:32 +0530

    UUID integration

--- src/main/java/com/example/productservice/models/BaseModel.java
+++ src/main/java/com/example/productservice/models/BaseModel.java
@@ -1,15 +1,19 @@
 package com.example.productservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(generator = "generator_name")
+    @GenericGenerator(name="generator_name",strategy = "uuid2")
+    @Column(name="id",columnDefinition ="binary(16)",nullable = false,updatable = false)
+    private UUID id;
 }
